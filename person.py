b7bd1ba3565df961adab6033bbdd05bf8080f729
Guilherme Beltrao <39063768+GuilhermeBeltrao@users.noreply.github.com>
2021-01-10 18:12:58 -0300

    Update person.py

--- person.py
+++ person.py
@@ -1,12 +1,30 @@
+import random
+
 from pokemon import WaterTypePokemon, FireTypePokemon, EletricTypePokemon
 
+
+NAMES = ['Gary', 'Dave', 'Bob', 'Donald', 'Tracy', 'Megan', 'Susan']
+
+POKEMONS = [
+    #   Fire pokemons
+    FireTypePokemon('Flareon'), FireTypePokemon('Charmander'), 
+    FireTypePokemon('Cyndaquil'), FireTypePokemon('Chimchar'),
+    
+    #   Water pokemons
+    WaterTypePokemon('Squirtle'), WaterTypePokemon('Psyduck'),
+    WaterTypePokemon('Horsea'), WaterTypePokemon('Croconaw'),
+    
+    #   Eletric pokemons
+    EletricTypePokemon('Pikachu'), EletricTypePokemon('Elekid'),
+    EletricTypePokemon('Shinx'), EletricTypePokemon('Voltorb')
+]
 class Person:
     
     def __init__(self, name=None, pokemons=[]):
         if name:
             self.name = name
         else: 
-            self.name = 'Desconhecido'
+            self.name = random.choice(NAMES)
         
         self.pokemons = pokemons
     
@@ -14,21 +32,58 @@ class Person:
         return self.name
     
     def show_pokemons(self):
-        for pokemon in self.pokemons:
-            print(pokemon)
+        if self.pokemons:
+            print('pokemons de {}'.format(self))
+            for i, pokemon in enumerate(self.pokemons):   
+                print('{} - {}'.format(i, pokemon))
+        else:
+            print('{} nao tem nenhum pokemon'.format(self))
             
+    def choose_pokemon(self):
+        self.show_pokemons()
+        if self.pokemons:
+            while True:
+                try:
+                    choice = input('escolha seu pokemon: ')
+                    choice = int(choice)
+                    chosen_pokemon = self.pokemons[choice]
+                    return chosen_pokemon
+                except:
+                    print('escolha invalida')
+        else:
+            print('este jogador nao possui nenhum pokemon para batalhar')
+    def battle(self, person):
+        print('{} desafiou {} para uma batalha!'.format(self, person))
+        person.show_pokemons()
+        self.choose_pokemon()
+        
+        while True:
+            choice = input('Escolha seu pokemon: ')
 
+            if choice == '1':
+                pass
+            elif choice == '2':
+                pass
+            elif choice == '3':
+                pass
+        
 class Player(Person):
     type = 'player'
-
+    
+    def capture_pokemon(self, pokemon):
+        self.pokemons.append(pokemon)
+        print("{} capturou {}".format(self, pokemon))
+        
 class Enemy(Person):
     type = 'enemy'
 
-player_pokemon1 = EletricTypePokemon('pikachu', 1)
-player_pokemon2 = WaterTypePokemon('squirtle', 2)
-player_pokemon3 = FireTypePokemon('charmander', 1)
+    def __init__(self, name=None, pokemons=[]):
+        super().__init__(name=name, pokemons=pokemons) 
+        #   super() puxa a funcao init de pessoa, para nao repetir codigo
 
-player = Player(name='Guilherme', pokemons=[player_pokemon1, player_pokemon2, player_pokemon3])
-
-print(player)
-player.show_pokemons()
+        if not pokemons:
+            for i in range(random.randint(1,6)):
+                pokemons.append(random.choice(POKEMONS))
+                #   escolhe entre 1 e 6 pokemons aleatorios 
+                #   e adiciona a lista de pokemons do inimigo
+    
