bca9f3d5c29bbea6bbebbc342002fe230b9e22b6
Guilherme Beltrao <39063768+GuilhermeBeltrao@users.noreply.github.com>
2021-01-13 00:14:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,62 @@
+import pickle
 from pokemon import Pokemon, WaterTypePokemon, FireTypePokemon, EletricTypePokemon
 from person import Person, Player, Enemy
 
-def choose_first_pokemon(player):
-    print('Ola {}! Escolha o pokemon que ira te acompanhar em sua jornada'.format(player))
-    
-    pikachu_inicial = EletricTypePokemon('Pikachu', level=1)
-    charmander_inicial = FireTypePokemon('Charmander', level=1)
-    squirtle_inicial = WaterTypePokemon('Squirtle', level=1)
-    print('Voce possui 3 escolhas de pokemon')
-    print('1 - ', pikachu_inicial)
-    print('2 - ', charmander_inicial)
-    print('3 - ', squirtle_inicial)
+def save_game(player):
+    try:
+        with open('pokemon-game/database.db', 'wb') as file:
+            pickle.dump(player, file)
+            print('O jogo foi salvo com sucesso')
+    except Exception as error:
+        print('erro ao salvar jogo: {}'.format(error))
 
+def load_game():
+    try:
+        with open('pokemon-game/database.db', 'rb') as file:
+            player = pickle.load(file)
+            return player
+    except Exception as error:
+        print('erro ao carregar jogo: {} '.format(error))
+
+if __name__ == '__main__':
+    print('-----------------------------------------')
+    print('Bem vindo ao pokemon RPG de terminal')
+    print('-----------------------------------------')
+    player = load_game() 
+    if not player:
+        name = input('Digite seu nome para comecar: ')
+        player = Player(name)
+        print('Ola {}, este e um mundo habitado por pokemons, sua missao e se tornar um mestre pokemon'.format(player))
+        print('Capture o maximo de pokemons que conseguir e batalhe contra seus inimigos')
+        print('')
+        
+        print("Você não tem nenhum pokemon, portanto precisa escolher um")
+        player.choose_first_pokemon() 
+        
+        print("Pronto, agora que você já possui um pokemon, enfrente seu arqui-rival desde o jardim da infância Gary")
+        gary = Enemy(name='Gary', pokemons=[WaterTypePokemon('Squirtle', level=1)])
+        player.battle(gary)
+        save_game(player)
+        
     while True:
-        choice = input('Escolha seu pokemon: ')
-
-        if choice == '1':
-            player.capture_pokemon(pikachu_inicial)
+        print("--------------------------------------")
+        print("O que deseja fazer?")
+        print("1 - Explorar o mapa")
+        print("2 - Lutar com um inimigo")
+        print("3 - Ver seus Pokemons")
+        print("0 - Sair do jogo")
+        choice = input("Sua escolha: ")
+        
+        if choice == '0':
+            print('Fechando jogo...')
             break
+        elif choice == '1':
+            player.explore_map()
+            save_game(player)
         elif choice == '2':
-            player.capture_pokemon(charmander_inicial)
-            break
+            random_enemy = Enemy()
+            player.battle(random_enemy)
+            save_game(player)
         elif choice == '3':
-            player.capture_pokemon(squirtle_inicial)
-            break
-player = Player('Guilherme')
-player.capture_pokemon(FireTypePokemon('Charmander', level=1))
-
-test_enemy = Enemy(name=None, pokemons=[WaterTypePokemon('Squirtle', level=1)])
-
-player.battle(test_enemy)
+            player.show_pokemons()
+        
